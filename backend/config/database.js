a1e4edbd5dfb9d68381531e8087a8d4adb7e295a
Aaron <aaron.hanson.brb@gmail.com>
2021-03-09 12:54:45 -0600

    fixed db config

--- backend/config/database.js
+++ backend/config/database.js
@@ -15,6 +15,11 @@ module.exports = {
     host,
     dialect: 'postgres',
     seederStorage: "sequelize",
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
     ssl: true,
     dialectOptions: {
       ssl: {
@@ -22,10 +27,5 @@ module.exports = {
         rejectUnauthorized: false // This line will fix new error
       }
     },
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
   },
 };
